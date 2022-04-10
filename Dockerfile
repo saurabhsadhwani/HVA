79e05de2b1473cf28f11444e4ffa198c740d0091
3D Bubble <70505719+3DBubble@users.noreply.github.com>
2022-04-10 20:38:17 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM python:3.8.13-buster AS BASE
+
+RUN apt-get update \
+    && apt-get --assume-yes --no-install-recommends install \
+        build-essential \
+        curl \
+        git \
+        jq \
+        libgomp1 \
+        vim
+
+WORKDIR /app
+
+# upgrade pip version
+RUN pip install --no-cache-dir --upgrade pip
+
+RUN pip install rasa==3.0.7
+RUN pip install pandas spacy openpyxl
+
+ADD config.yml config.yml
+ADD domain.yml domain.yml
+ADD credentials.yml credentials.yml
+ADD endpoints.yml endpoints.yml
+ADD dataset.xlsx dataset.xlsx
+ADD dimensionsRandomForest.pkl dimensionsRandomForest.pkl
+ADD disease.pkl disease.pkl
+ADD printer.py printer.py
+ADD randomForest.pkl randomForest.pkl
+ADD SymptomsDiagnosis.py SymptomsDiagnosis.py
---
+++ printer.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+from typing import Dict, Text, Any, List
+
+from rasa.engine.graph import GraphComponent, ExecutionContext
+from rasa.engine.recipes.default_recipe import DefaultV1Recipe
+from rasa.engine.storage.resource import Resource
+from rasa.engine.storage.storage import ModelStorage
+from rasa.shared.nlu.training_data.message import Message
+from rasa.shared.nlu.training_data.training_data import TrainingData
+
+print('Printmsg initialized')
+
+@DefaultV1Recipe.register(
+    [DefaultV1Recipe.ComponentType.MESSAGE_FEATURIZER], is_trainable=False
+)
+class printmsg(GraphComponent):
+
+    @classmethod
+    def create(
+        cls,
+        config: Dict[Text, Any],
+        model_storage: ModelStorage,
+        resource: Resource,
+        execution_context: ExecutionContext,
+    ) -> GraphComponent:
+        # TODO: Implement this
+        pass
+    
+    def train(self, training_data: TrainingData) -> Resource:
+        # TODO: Implement this if your component requires training
+        pass
+    
+    def process_training_data(self, training_data: TrainingData) -> TrainingData:
+        # TODO: Implement this if your component augments the training data with
+        #       tokens or message features which are used by other components
+        #       during training.
+        # components during training.
+
+        return training_data
+    
+    def process(self, messages: List[Message]) -> List[Message]:
+        # TODO: This is the method which Rasa Open Source will call during inference.
+        for message in messages:
+            print(message.data)
+
+        return messages
